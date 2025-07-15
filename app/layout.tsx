153ef93df4e7b0fd9f5ce0204bf81ce69a380728
Denis <f104378@students.nbu.bg>
2025-07-15 23:55:19 +0300

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Portfolio',
+  description: 'Denis Stankov Portfolio',
+  generator: 'Denis Stankov',
 }
 
 export default function RootLayout({
